901d09d264ae8eb93cfcf3a9066ced3febd57758
CemYilmaz <cemyilmaz463@gmail.com>
2022-12-01 15:50:27 +0300

    Added JsonSerializable to API models

--- packages/open_meteo_api/lib/src/models/weather.dart
+++ packages/open_meteo_api/lib/src/models/weather.dart
@@ -1,6 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'weather.g.dart';
+
+@JsonSerializable()
 class Weather {
   final double temperature;
   final double weathercode;
 
   const Weather({required this.temperature, required this.weathercode});
+
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
 }
